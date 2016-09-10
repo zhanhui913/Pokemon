d58513f7b56d2b460c027093b9a577f6791ebf33
zhanhui913 <zhanhui913@gmail.com>
2016-09-09 18:28:22 -0700

    Testing with different json data

--- data.json.js
+++ data.json.js
@@ -1,3 +1,3 @@
 {
-	"name":"Zhan"
+	"name" : "Zhan1"
 }
--- loader.js
+++ loader.js
@@ -1,15 +1,15 @@
 (function($){
-			$(document).ready(function(){
-				$.ajax({
-  					url: "http://zhanhui913.github.io/Pokemon/data.json.js",
-  					dataType: "jsonp",
-  					jsonpCallback: "zhanhui913/Pokemon:data",
-  					success: function(data) {
-  						console.log("success");
-  					},
-    				error: function() { // callback if there's an error
-      				console.log("error");
-    				}
-				});
-			});
+    $(document).ready(function(){
+        $.ajax({
+            url: "http://zhanhui913.github.io/Pokemon/data.json.js",
+            dataType: "jsonp",
+            jsonpCallback: "zhanhui913/Pokemon:data",
+            success: function(data) {
+                console.log("success");
+            },
+            error: function() { // callback if there's an error
+                console.log("error");
+            }
+        });
+    });
 })(jQuery);
