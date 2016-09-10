939f1782b3187c6882491b6351ea1afa045ad60a
zhanhui913 <zhanhui913@gmail.com>
2016-09-10 12:53:20 -0700

    Add relevant data into data.json.js

--- loader.js
+++ loader.js
@@ -9,9 +9,25 @@
             timeout: 5000,
             crossDomain: true,
             success: function(json) {
-                console.log("success : ");
+
+                var $content = $("#pokemonContent");
+
+                console.log("success");
                 $.each(json, function(i, el) {
                     console.log(el.name+" has "+el.img.length+" images");
+
+
+                    
+                    //$content.append("<!-- " + el.name + " -->\n<div class='container'><div class='row'>" + el.name + "</div></div>\n");
+                    
+                    var img = (el.img.length > 0) ? "images/sample.jpg" : "images/placeholder.png";
+                    var description = (el.img.length > 0) ? el.img[0].description : "NA";
+                    var date = (el.date == null) ? "NA" : el.date;
+
+                    var s = "<!--" + el.name + "-->\n<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'>\n<a href=" + img + " class='pop-up'>\n<div class='portfolio-item'>\n<div class='portfolio-item-preview'>\n<img src=" + img + " alt=''>\n<div class='hidden ptitle'>\n" + description + "\n</div>\n</div>\n<div class='portfolio-item-description'>\n<h3>" + el.name + "</h3>\n<p>" + date + "</p>\n</div>\n</div>\n</a>\n</div>\n";
+
+                    $content.append(s);
+
                 });
             },
             error: function (request, textStatus, errorThrown) {
