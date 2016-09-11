7a9f3eb3a90ed18097540a097d80020fd1ed283a
zhanhui913 <zhanhui913@gmail.com>
2016-09-10 18:42:15 -0700

    Make small changes to js and css:

--- loader.js
+++ loader.js
@@ -13,6 +13,7 @@
                 var $content = $("#pokemonContent");
 
                 console.log("success");
+                //bootstrap
                 $.each(json, function(i, el) {
                     console.log(el.name+" has "+el.img.length+" images");
 
@@ -21,10 +22,31 @@
                     var description = (el.img.length > 0) ? el.img[0].description : "NA";
                     var date = (el.date == null) ? "NA" : el.date;
 
-                    var s = "<!--" + el.name + "-->\n<div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>\n<a href=" + img + " class='pop-up'>\n<div class='portfolio-item'>\n<div class='portfolio-item-preview'>\n<img src=" + img + " alt=''>\n<div class='hidden ptitle'>\n" + description + "\n</div>\n</div>\n<div class='portfolio-item-description'>\n<h3>" + el.name + "</h3>\n<p>" + date + "</p>\n</div>\n</div>\n</a>\n</div>\n";
+
+
+                    var  imgObj = new Image();
+                    imgObj.src = img;
+                    var h = imgObj.height;
+                    var w = imgObj.width;
+
+
+                    var s = "<!--" + el.name + " ("+w+","+h+ ") -->\n<div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>\n<a href=" + img + " class='pop-up'>\n<div class='portfolio-item'>\n<div class='portfolio-item-preview'>\n<img src=" + img + " alt=''>\n<div class='hidden ptitle'>\n" + description + "\n</div>\n</div>\n<div class='portfolio-item-description'>\n<h3>" + el.name + "</h3>\n<p>" + date + "</p>\n</div>\n</div>\n</a>\n</div>\n";
+
+
+
+
+
 
                     $content.append(s);
+
+
+                    //put this on every new  row
+                    "<div class='clearfix col-lg-12 visible-lg'></div>"
+
                 });
+
+
+
             },
             error: function (request, textStatus, errorThrown) {
                 console.log(request.responseText);
